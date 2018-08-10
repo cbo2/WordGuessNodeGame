ba9177e1dc6a988cb05981ea08615793da88373e
Craig Bomba <talk2cbo@gmail.com>
2018-08-10 08:38:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+
+var Word = require("./word.js");
+var inquirer = require("inquirer");
+
+// Randomly selects a word and uses the Word constructor to store it
+
+var word = new Word("hello");
+word.start();
+console.log(word.getWord());
+
+// Prompts the user for each guess and keeps track of the user's remaining guesses
+playRound();
+
+function playRound() {
+
+    // We create a list prompt. Specifying that the user must pick a random number between 1 and 5.
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "userGuess",
+        message: "Guess a letter [a-z]",
+        validate: function(value) {   
+            // test for only lower case a-z or upper case A-Z
+            if (/[a-z]/i.test(value)) {  
+              return true;
+            }
+            return false;   // returning false indicates to keep asking for input 
+          }
+      }
+  
+    ]).then(function(guess) {
+        console.log("The user guessed: " + guess.userGuess);
+        word.guess(guess.userGuess);
+        console.log(word.getWord());
+        checkStatus();
+    });
+}
+
+function checkStatus() {
+    // check if number of guesses exceeds a threshold
+    playRound();
+}
---
+++ word.js
@@ -0,0 +1,45 @@
+// Word.js: Contains a constructor, Word that depends on the Letter constructor. This is used to create 
+// an object representing the current word the user is attempting to guess. That means the constructor 
+// should define:
+
+
+// An array of new Letter objects representing the letters of the underlying word
+// A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+// A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+var Letter = require("./letter.js");
+
+function Word(letterArray) {
+    this.originalLetters = letterArray;
+    this.letters = [];
+
+    this.start = function() {
+        for (var i = 0; i < this.originalLetters.length; i++) {
+            this.letters[i] = new Letter(this.originalLetters[i]);
+        }
+    } 
+}
+
+Word.prototype.guess = function(letterGuessed) {
+    for (var i = 0; i < this.letters.length; i++) {
+        this.letters[i].checkLetter(letterGuessed);
+    }
+}
+
+Word.prototype.getWord = function() {
+    var returnWord = "";
+    for (var i = 0; i < this.letters.length; i++) {
+        // returnWord += this.letters[i].toString();
+        returnWord += this.letters[i];
+    }
+    return returnWord;
+}
+
+var letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// test();
+function test() {
+    var word = new Word("hello");
+    word.start();
+    console.log(word.getWord());
+}
+
+module.exports = Word;
